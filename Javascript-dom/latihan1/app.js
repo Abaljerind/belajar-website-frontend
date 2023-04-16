05e59e386e883ed8ac74e8801245f76329101316
Abaljerind <abaljerinda@gmail.com>
2023-04-16 22:13:25 +0700

    edit: contoh getElementById()

---
+++ Javascript-dom/latihan1/app.js
@@ -0,0 +1,8 @@
+// dom selection
+// document.getElementById()
+const judul = document.getElementById("judul");
+judul.style.color = "salmon";
+judul.style.backgroundColor = "#ccc";
+judul.innerHTML = "naruto uzumaki";
+
+// document.getElementsByTagName()
