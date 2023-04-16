96a043f92b7bdfbd357fea6efd5187b3982b5f0a
Abaljerind <abaljerinda@gmail.com>
2023-04-16 22:22:40 +0700

    edit: contoh getElementsByTagName()

--- Javascript-dom/latihan1/app.js
+++ Javascript-dom/latihan1/app.js
@@ -6,3 +6,16 @@ judul.style.backgroundColor = "#ccc";
 judul.innerHTML = "naruto uzumaki";
 
 // document.getElementsByTagName()
+//  -> HTMCollections
+const p = document.getElementsByTagName("p");
+
+for (let i = 0; i < p.length; i++) {
+  p[i].style.backgroundColor = "lightblue";
+}
+
+// cara mengakali penggunaan getElementsByTagName agar tidak menghasilkan HTMLCollection, bisa dengan menambahkan index nya seperti dibawah ini.
+const h1 = document.getElementsByTagName("h1")[0];
+h1.style.fontSize = "50px";
+
+// document.getElementsByClassName()
+// -> HTMLCollections
