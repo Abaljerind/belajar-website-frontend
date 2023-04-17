4808df1dc63ab9536847a3a5ed81be73a1cd9e12
Abaljerind <abaljerinda@gmail.com>
2023-04-17 22:20:50 +0700

    add: contoh style

--- Javascript-dom/latihan1/app3.js
+++ Javascript-dom/latihan1/app3.js
@@ -1,5 +1,11 @@
-const judul = document.getElementById("judul");
-judul.innerHTML = "<em>naruto uzumaki</em>";
+// manipulation innerHTML
+// const judul = document.getElementById("judul");
+// judul.innerHTML = "<em>naruto uzumaki</em>";
 
-const sectionA = document.querySelector("section#a");
-sectionA.innerHTML = "hello world!";
+// const sectionA = document.querySelector("section#a");
+// sectionA.innerHTML = "hello world!";
+
+// manipulation style
+const judul = document.querySelector("#judul");
+judul.style.color = "salmon";
+judul.style.backgroundColor = "lightblue";
