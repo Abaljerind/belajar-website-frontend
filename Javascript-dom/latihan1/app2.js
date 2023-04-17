59239bb75ef8e6106676f763c29f2ca68c3bee82
Abaljerind <abaljerinda@gmail.com>
2023-04-17 21:53:14 +0700

    add: contoh querySelector()

--- Javascript-dom/latihan1/app2.js
+++ Javascript-dom/latihan1/app2.js
@@ -1,2 +1,7 @@
 // document.querySelector()
 const p4 = document.querySelector("#b p");
+p4.style.color = "green";
+p4.style.fontSize = "30px";
+
+const li2 = document.querySelector("section#b ul li:nth-child(2)");
+li2.style.backgroundColor = "orange";
