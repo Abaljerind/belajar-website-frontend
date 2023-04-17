fd75dd65604696749c80bb815691c5cf8a9bab83
Abaljerind <abaljerinda@gmail.com>
2023-04-17 21:56:47 +0700

    add: contoh querySelectorAll()

--- Javascript-dom/latihan1/app2.js
+++ Javascript-dom/latihan1/app2.js
@@ -5,3 +5,13 @@ p4.style.fontSize = "30px";
 
 const li2 = document.querySelector("section#b ul li:nth-child(2)");
 li2.style.backgroundColor = "orange";
+
+// document.querySelectorAll() -> nodelist
+// karna nodelist nilai return-nya, maka kita perlu tambahkan index untuk penggunaannya.
+const p = document.querySelectorAll("p");
+
+// p[2].style.backgroundColor = "lightblue";
+
+for (let i = 0; i < p.length; i++) {
+  p[i].style.backgroundColor = "lightblue";
+}
