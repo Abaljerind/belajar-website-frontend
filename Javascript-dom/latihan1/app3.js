2245f300f456ea55bcdfa695000053fb93fd14fb
Abaljerind <abaljerinda@gmail.com>
2023-04-17 22:26:58 +0700

    add: contoh set & get attribute

--- Javascript-dom/latihan1/app3.js
+++ Javascript-dom/latihan1/app3.js
@@ -6,6 +6,17 @@
 // sectionA.innerHTML = "hello world!";
 
 // manipulation style
-const judul = document.querySelector("#judul");
-judul.style.color = "salmon";
-judul.style.backgroundColor = "lightblue";
+// const judul = document.querySelector("#judul");
+// judul.style.color = "salmon";
+// judul.style.backgroundColor = "lightblue";
+
+// manipulation attribute
+// bisa juga dilakukan di console browser tetapi hasilnya akan hilang saat di reload page nya
+const judul = document.getElementsByTagName("h1")[0];
+judul.setAttribute("name", "naruto");
+
+const a = document.querySelector("section#a a");
+a.setAttribute("id", "link");
+
+console.log(a.getAttribute("href"));
+console.log(judul.getAttribute("id"));
