adb16beffd927cb4bd77a95882c35f15810c1000
Abaljerind <abaljerinda@gmail.com>
2023-04-17 22:18:28 +0700

    add: contoh innerHTML

---
+++ Javascript-dom/latihan1/app3.js
@@ -0,0 +1,5 @@
+const judul = document.getElementById("judul");
+judul.innerHTML = "<em>naruto uzumaki</em>";
+
+const sectionA = document.querySelector("section#a");
+sectionA.innerHTML = "hello world!";
