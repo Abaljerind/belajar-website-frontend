0f5f6f64ea198e2f6c6e08258b2d4f94a2aceb73
Abaljerind <abaljerinda@gmail.com>
2023-04-17 22:38:07 +0700

    add: contoh classList add, remove, toggle

--- Javascript-dom/latihan1/app3.js
+++ Javascript-dom/latihan1/app3.js
@@ -12,14 +12,24 @@
 
 // manipulation attribute
 // bisa juga dilakukan di console browser tetapi hasilnya akan hilang saat di reload page nya
-const judul = document.getElementsByTagName("h1")[0];
-judul.setAttribute("name", "naruto");
+// const judul = document.getElementsByTagName("h1")[0];
+// judul.setAttribute("name", "naruto");
 
-const a = document.querySelector("section#a a");
-a.setAttribute("id", "link");
+// const a = document.querySelector("section#a a");
+// a.setAttribute("id", "link");
 
-console.log(a.getAttribute("href"));
-console.log(judul.getAttribute("id"));
+// console.log(a.getAttribute("href"));
+// console.log(judul.getAttribute("id"));
 
 // menghilangkan attribute
 // console.log(a.removeAttribute('href'))
+
+// manipulasi class dengan classList
+const p2 = document.querySelector(".p2");
+// console.log(p2.classList.add("label"));
+// console.log(p2.classList.remove("label"));
+
+// console.log(p2.classList.toggle("label"));
+// true, kalau tidak ada class label & akan ditambahkan class label
+// console.log(p2.classList.toggle("label"));
+// false, kalau ada class label & akan dihapuskan class label
