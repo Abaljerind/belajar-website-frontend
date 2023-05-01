5508424bd71e8e07d25f5f2635f5882a80339478
Abaljerind <abaljerinda@gmail.com>
2023-05-01 10:37:54 +0700

    add: notes & function

--- Javascript-dom/latihan1/app5.js
+++ Javascript-dom/latihan1/app5.js
@@ -1,10 +1,17 @@
 // DOM Events
 // ada 2 tipe:
-// 1. Event Handler ->
-// a. inline html attribute
-// b. element method
+// 1. Event Handler -> ada 2
+// a. inline html attribute, disimpan sebagai atribute html
+// b. element method, disimpan sebagai method
 // 2. addEventListener()
 
 // ---------------------------------------------------------------
 
 // event handler -> on<event>, contoh: onclick
+// memberikan warna background pada p3
+const p3 = document.querySelector(".p3");
+
+// membuat function
+function ubahWarna() {
+  p3.style.backgroundColor = "lightblue";
+}
