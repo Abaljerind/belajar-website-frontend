e81ffe2c92d03c3de65d77015b3aae3e7f266e39
Abaljerind <abaljerinda@gmail.com>
2023-05-01 09:23:56 +0700

    add: notes & contoh create new element

--- Javascript-dom/latihan1/app4.js
+++ Javascript-dom/latihan1/app4.js
@@ -2,9 +2,33 @@
 
 // --------------------------------------------------------------
 // Manipulasi Node: ada banyak caranya tapi yang akan dibahas hanya berikut:
-// document.createElement()
-// document.createTextNode()
-// node.appendChild()
-// node.insertBefore()
+
+// document.createElement() -> membuat elemen
+
+// document.createTextNode() -> menuliskan apa di dalam node / elemen yang kita buat
+
+// node.appendChild() -> menyimpan / menggabungkan text yang dibuat dengan elemen html yg baru kita buat / dengan elemen dimana kita ingin menambahkan elemen baru nya berada setelah elemen terakhir.
+
+// node.insertBefore() -> sama dengan appendChild diatas, bedanya posisi elemen / text yang kita ingin gabungkan berada sebelum elemen lain, sesuai namanya "insert before".
+
 // parentNode.removeChild()
 // parentNode.replaceChild()
+
+// teori pembuatan node => kita buat elemennya dulu, lalu buat isinya berupa teks, lalu kita minta js untuk memasukkan teks tersebut ke dalam elemen dan menyimpannya di dalam body yang kita inginkan.
+
+// --------------------------------------------------------------
+
+// buat elemen baru
+const pBaru = document.createElement("p");
+
+// buat tulisan yang akan disimpan ke dalam pBaru
+const teksPBaru = document.createTextNode("Paragraf baru");
+
+// simpan tulisan diatas ke dalam pBaru
+pBaru.appendChild(teksPBaru);
+
+// simpan pBaru yang sudah ditambahkan teksPBaru dengan node.appendChild() or node.insertBefore()
+const sectionA = document.querySelector("section#a");
+
+sectionA.appendChild(pBaru);
+// done, pBaru akan muncul dengan teks Paragraf baru.
