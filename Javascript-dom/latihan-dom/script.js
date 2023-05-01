b830a63ce238ca8f80710a237d6dda960bd91967
Abaljerind <abaljerinda@gmail.com>
2023-05-01 14:05:27 +0700

    add button baru + event acak warna

--- Javascript-dom/latihan-dom/script.js
+++ Javascript-dom/latihan-dom/script.js
@@ -8,3 +8,20 @@ tUbahWarna.addEventListener("click", function () {
 });
 
 // untuk menggunakan toggle, kita perlu memainkan function classList.toggle, tapi pertama tama kita perlu membuat class nya dulu di dalam css.
+
+// add button baru
+const tAcakWarna = document.createElement("button");
+const teksTombol = document.createTextNode("Acak Warna");
+
+tAcakWarna.appendChild(teksTombol);
+tAcakWarna.setAttribute("type", "button");
+
+tUbahWarna.after(tAcakWarna);
+
+// tambah event
+tAcakWarna.addEventListener("click", function () {
+  const r = Math.round(Math.random() * 255) + 1;
+  const g = Math.round(Math.random() * 255) + 1;
+  const b = Math.round(Math.random() * 255) + 1;
+  document.body.style.backgroundColor = `rgb(${r},${g},${b})`;
+});
