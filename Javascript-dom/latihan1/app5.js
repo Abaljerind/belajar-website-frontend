3870fa808aab7592f9a662561402b0df66f16004
Abaljerind <abaljerinda@gmail.com>
2023-05-01 10:32:21 +0700

    create: app5.js, edit: src script

---
+++ Javascript-dom/latihan1/app5.js
@@ -0,0 +1,10 @@
+// DOM Events
+// ada 2 tipe:
+// 1. Event Handler ->
+// a. inline html attribute
+// b. element method
+// 2. addEventListener()
+
+// ---------------------------------------------------------------
+
+// event handler -> on<event>, contoh: onclick
