bf146787d965e5200e992b32d600ca23d915409f
Abaljerind <abaljerinda@gmail.com>
2023-05-01 13:41:57 +0700

    add function toggle dari classlist

--- Javascript-dom/latihan-dom/script.js
+++ Javascript-dom/latihan-dom/script.js
@@ -4,6 +4,7 @@ const tUbahWarna = document.getElementById("tUbahWarna");
 // tambah addEventListener untuk ubah warna background + toggle
 tUbahWarna.addEventListener("click", function () {
   //   document.body.style.backgroundColor = "lightskyblue";
+  document.body.classList.toggle("biru-langit");
 });
 
-// untuk menggunakan toggle, kita perlu memainkan function classList.add & remove, tapi pertama tama kita perlu membuat class nya dulu di dalam css.
+// untuk menggunakan toggle, kita perlu memainkan function classList.toggle, tapi pertama tama kita perlu membuat class nya dulu di dalam css.
