fd793a1627ce4bba7e26ad531923cb5cda56f811
Abaljerind <abaljerinda@gmail.com>
2023-05-01 11:23:29 +0700

    add note & event listener

---
+++ Javascript-dom/latihan-dom/script.js
@@ -0,0 +1,9 @@
+// tangkap button
+const tUbahWarna = document.getElementById("tUbahWarna");
+
+// tambah addEventListener untuk ubah warna background + toggle
+tUbahWarna.addEventListener("click", function () {
+  //   document.body.style.backgroundColor = "lightskyblue";
+});
+
+// untuk menggunakan toggle, kita perlu memainkan function classList.add & remove, tapi pertama tama kita perlu membuat class nya dulu di dalam css.
