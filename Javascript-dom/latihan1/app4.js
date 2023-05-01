e86f546f24cb8b0b74e0486a7a597065026364b5
Abaljerind <abaljerinda@gmail.com>
2023-05-01 09:08:58 +0700

    create: app4.js & ubah src script html

---
+++ Javascript-dom/latihan1/app4.js
@@ -0,0 +1,10 @@
+// DOM MANIPULATION part 2
+
+// --------------------------------------------------------------
+// Manipulasi Node: ada banyak caranya tapi yang akan dibahas hanya berikut:
+// document.createElement()
+// document.createTextNode()
+// node.appendChild()
+// node.insertBefore()
+// parentNode.removeChild()
+// parentNode.replaceChild()
