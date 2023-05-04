468821afb091e7af10cfe3e5f15b2cf1298e2a98
Abaljerind <abaljerinda@gmail.com>
2023-05-04 13:10:20 +0700

    add func. reset & win point opt

--- Javascript-dom/latihan2-codepolitan/latihan-scoreboard/app.js
+++ Javascript-dom/latihan2-codepolitan/latihan-scoreboard/app.js
@@ -2,11 +2,13 @@ const p1Button = document.getElementById("p1-button");
 const p2Button = document.getElementById("p2-button");
 const p1Display = document.getElementById("p1-display");
 const p2Display = document.getElementById("p2-display");
+const resetButton = document.getElementById("reset");
+const winPointOption = document.getElementById("winpoint");
 
 let p1Score = 0;
 let p2Score = 0;
 
-let winPoint = 5;
+let winPoint = 3;
 let isGameOver = false;
 
 p1Button.addEventListener("click", function () {
@@ -27,4 +29,23 @@ p2Button.addEventListener("click", function () {
     }
     p2Display.textContent = p2Score;
   }
+});
+
+// function reset
+function reset() {
+  p1Score = 0;
+  p2Score = 0;
+
+  p1Display.textContent = 0;
+  p2Display.textContent = 0;
+
+  isGameOver = false;
+}
+
+resetButton.addEventListener("click", reset);
+
+// winPointOption
+winPointOption.addEventListener("change", function () {
+  winPoint = parseInt(this.value);
+  reset();
 });
