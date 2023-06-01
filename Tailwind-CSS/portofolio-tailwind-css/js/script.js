aaafdab17d4b93344b474624f0c0ae467e0db3ca
Abaljerind <abaljerinda@gmail.com>
2023-06-01 11:41:35 +0700

    add js for klik diluar hamburger

--- Tailwind-CSS/portofolio-tailwind-css/js/script.js
+++ Tailwind-CSS/portofolio-tailwind-css/js/script.js
@@ -22,3 +22,11 @@ hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("hamburger-active");
   navMenu.classList.toggle("hidden");
 });
+
+// Klik diluar hamburger
+window.addEventListener("click", function (e) {
+  if (e.target != hamburger && e.target != navMenu) {
+    hamburger.classList.remove("hamburger-active");
+    navMenu.classList.add("hidden");
+  }
+});
