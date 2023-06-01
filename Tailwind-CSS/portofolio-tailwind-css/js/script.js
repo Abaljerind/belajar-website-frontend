de7dbd2627b647d41fbc0f3c178516c296655bcd
Abaljerind <abaljerinda@gmail.com>
2023-06-01 13:25:11 +0700

    add script to js to keep theme in localStorage

--- Tailwind-CSS/portofolio-tailwind-css/js/script.js
+++ Tailwind-CSS/portofolio-tailwind-css/js/script.js
@@ -38,7 +38,9 @@ const html = document.querySelector("html");
 darkToggle.addEventListener("click", function () {
   if (darkToggle.checked) {
     html.classList.add("dark");
+    localStorage.theme = "dark";
   } else {
     html.classList.remove("dark");
+    localStorage.theme = "light";
   }
 });
