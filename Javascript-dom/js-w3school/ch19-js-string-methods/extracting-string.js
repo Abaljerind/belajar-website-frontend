b8aefcac08d0793c396884dc9543650991b7e049
Abaljerind <abaljerinda@gmail.com>
2023-09-24 10:17:38 +0700

    string-methods slice

---
+++ Javascript-dom/js-w3school/ch19-js-string-methods/extracting-string.js
@@ -0,0 +1,14 @@
+// slice(start, end)
+let text = "apple, banana, kiwi";
+let banana = text.slice(7, 13);
+
+let kiwi = text.slice(15, 19);
+
+let apple = text.slice(0, 5);
+
+let text1 = "sepeda, motor, mobil";
+let sepeda = text1.slice(0, 6);
+
+let motor = text1.slice(8, 13);
+
+let mobil = text1.slice(15, 20);
