a8fdad4a5b5d438303c2c6acb6c81380e4041123
Abaljerind <abaljerinda@gmail.com>
2023-09-25 11:39:41 +0700

    contoh lastIndexOf() dan parameter kedua

--- Javascript-dom/js-w3school/ch20-js-string-search/indexOf.js
+++ Javascript-dom/js-w3school/ch20-js-string-search/indexOf.js
@@ -1,6 +1,15 @@
-// indexOf() -> search for position of string
+// indexOf() -> mencari kata yang cocok dengan yang diminta, dan mengembalikan posisi awalnya
 let text = 'Please locate where "locate" occurs!';
 let index1 = text.indexOf("locate");
 
 let index2 = text.indexOf("Please");
 let index3 = text.indexOf("where");
+
+// lastIndexOf() -> sama dengan indexOf() tapi posisi yang dikembalikan adalah posisi akhirnya, pencarian dimulai dari akhir kalimat.
+let index4 = text.lastIndexOf("Please");
+let index5 = text.lastIndexOf("locate");
+let index6 = text.lastIndexOf("occurs!");
+
+// parameter kedua disini adalah posisi dimulainya pencarian
+let index7 = text.lastIndexOf("locate", 15);
+let index8 = text.indexOf("locate", 15);
