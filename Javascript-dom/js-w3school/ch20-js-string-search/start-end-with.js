5b9a0613728eb12566d239ae9f4e0f01d75be956
Abaljerind <abaljerinda@gmail.com>
2023-09-25 13:37:22 +0700

    contoh startsWith();

---
+++ Javascript-dom/js-w3school/ch20-js-string-search/start-end-with.js
@@ -0,0 +1,6 @@
+// startsWith() -> mengembalikan true jika sebuah string dimulai dengan value yang dicari.
+let text = "Hello world, welcome to the universe.";
+text.startsWith("Hello");
+text.startsWith("hello");
+text.startsWith("universe");
+text.startsWith("welcome");
