2ac8aee1e994afe94c9d9a8548ec99508d40ee40
Abaljerind <abaljerinda@gmail.com>
2023-09-25 11:33:24 +0700

    contoh indexOf()

---
+++ Javascript-dom/js-w3school/ch20-js-string-search/indexOf.js
@@ -0,0 +1,6 @@
+// indexOf() -> search for position of string
+let text = 'Please locate where "locate" occurs!';
+let index1 = text.indexOf("locate");
+
+let index2 = text.indexOf("Please");
+let index3 = text.indexOf("where");
