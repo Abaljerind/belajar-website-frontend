707cd204700a8be0a6bd0bc5e70c89d1106b510c
Abaljerind <abaljerinda@gmail.com>
2023-09-25 13:32:38 +0700

    contoh includes()

---
+++ Javascript-dom/js-w3school/ch20-js-string-search/includes.js
@@ -0,0 +1,8 @@
+// includes() -> mengembalikan nilai true jika sebuah string berisikan value yang dicari.
+let text = "Hello world, welcome to the universe.";
+text.includes("world");
+text.includes("to the");
+text.includes("naruto");
+
+let text2 = "Hello World";
+text2.includes("orl");
