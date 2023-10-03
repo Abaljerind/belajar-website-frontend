2bfeab07a5bfd87a741af56e647dfc65f695288a
Abaljerind <abaljerinda@gmail.com>
2023-10-03 21:17:18 +0700

    add example of toString() & join

---
+++ Javascript-dom/js-w3school/ch27-js-array-methods/to-string.js
@@ -0,0 +1,5 @@
+const fruits = ["banana", "apple", "orange", "watermelon", "melon"];
+console.log(fruits.toString());
+
+// join, sama dengan toString() tetapi kita bisa menambahkan pemisah / seperator
+console.log(fruits.join(", "));
