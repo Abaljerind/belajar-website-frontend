9e35001f0b39ddec7cb5a230f11a2550b6306693
Abaljerind <abaljerinda@gmail.com>
2023-10-03 21:35:48 +0700

    sort() with numbers

--- Javascript-dom/js-w3school/ch28-js-array-sort/sort.js
+++ Javascript-dom/js-w3school/ch28-js-array-sort/sort.js
@@ -8,3 +8,11 @@ const fruits = [
   "mango",
 ];
 console.log(fruits.sort());
+
+// sort() dengan number harus menambahkan perbandingan seperti dibawah ini
+const points = [71, 30, 55, 90, 100, 76, 89, 12, 0, 44, 68, 65];
+console.log(
+  points.sort(function (a, b) {
+    return a - b;
+  })
+);
