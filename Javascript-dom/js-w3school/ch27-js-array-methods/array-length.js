26acc0f69353e69f97a76c03caf7ccf3356e14b8
Abaljerind <abaljerinda@gmail.com>
2023-10-03 21:11:03 +0700

    add array length example

--- Javascript-dom/js-w3school/ch27-js-array-methods/array-length.js
+++ Javascript-dom/js-w3school/ch27-js-array-methods/array-length.js
@@ -1,2 +1,7 @@
 const fruits = ["banana", "orange", "apple", "mango"];
 let size = fruits.length;
+
+const gadget = ["pc", "laptop", "smartphone", "smartwatch", "earphone"];
+let gadgetSize = gadget.length;
+
+// jumlah yang dihitung adalah jumlah keseluruhan isi di dalam array nya.
