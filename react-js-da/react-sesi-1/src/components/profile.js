7720a67a674b31ebe93ee62c94cd1a9b1f9a966d
Abaljerind <abaljerinda@gmail.com>
2023-11-16 21:54:44 +0700

    create profile.js, using class component

---
+++ react-js-da/react-sesi-1/src/components/profile.js
@@ -0,0 +1,16 @@
+// pada App.js dan introduction.js kita menggunakan yang namanya functional component
+// berikut ini contoh penggunaan class component
+import React from "react";
+
+class Profile extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Sesi 1 Belajar React JS</h1>
+        <p>ini adalah component Profile</p>
+      </div>
+    );
+  }
+}
+
+export default Profile;
