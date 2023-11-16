967a843fb383449bb354889a462722bd55507932
Abaljerind <abaljerinda@gmail.com>
2023-11-16 21:27:36 +0700

    import introduction.js components

--- react-js-da/react-sesi-1/src/App.js
+++ react-js-da/react-sesi-1/src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
+import Introduction from "./components/introduction";
 
 function App() {
   return (
     <div>
       <h1>Selamat datang di SESI 1 React JS</h1>
       <p>halo, saya sedang belajar react-js disini</p>
+      <Introduction />
+      <Introduction />
     </div>
   );
 }
