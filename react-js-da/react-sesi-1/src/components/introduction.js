73257cffe66bf64640597f6c457630fea50e0f70
Abaljerind <abaljerinda@gmail.com>
2023-11-16 21:27:07 +0700

    create introduction.js components

---
+++ react-js-da/react-sesi-1/src/components/introduction.js
@@ -0,0 +1,13 @@
+function Introduction() {
+  return (
+    <div>
+      <h1>Introduction</h1>
+      <p>
+        halo, nama saya naruto uzumaki, selamat anda telah sampai dipembelajaran
+        react js
+      </p>
+    </div>
+  );
+}
+
+export default Introduction;
