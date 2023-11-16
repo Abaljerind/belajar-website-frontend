c1e8c96739d4ebb38e4a87973fa582d48d5d3710
Abaljerind <abaljerinda@gmail.com>
2023-11-16 21:54:04 +0700

    delete some import code, delete div keyword, add Profile component

--- react-js-da/react-sesi-1/src/App.js
+++ react-js-da/react-sesi-1/src/App.js
@@ -1,16 +1,18 @@
-import "./App.css";
 import Introduction from "./components/introduction";
+import Profile from "./components/profile";
 
 function App() {
   return (
-    <div>
+    <>
       <h1>Selamat datang di SESI 1 React JS</h1>
       <p>halo, saya sedang belajar react-js disini</p>
+
+      <Profile />
 
       {/* mengirim data name dari App.js ke introduction.js */}
       <Introduction name="naruto uzumaki" />
       <Introduction name="hinata hyuga" />
-    </div>
+    </>
   );
 }
 
