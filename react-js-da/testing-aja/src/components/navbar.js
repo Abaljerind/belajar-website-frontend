999864a7777d0c50680e01a1a9fbdc981bdaf9ae
Abaljerind <abaljerinda@gmail.com>
2023-11-17 17:06:49 +0700

    create navbar components

---
+++ react-js-da/testing-aja/src/components/navbar.js
@@ -0,0 +1,19 @@
+function Navbar() {
+  return (
+    <div>
+      <ul>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Navbar;
