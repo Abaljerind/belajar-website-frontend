2d0a288878ee14354c8fa2a916d8f850bf202c23
Abaljerind <abaljerinda@gmail.com>
2023-11-17 19:23:30 +0700

    create constructor for props & add props with argument from App.js

--- react-js-da/testing-aja/src/components/footer.js
+++ react-js-da/testing-aja/src/components/footer.js
@@ -1,10 +1,16 @@
 import React from "react";
 
 class Footer extends React.Component {
+  // membuat constructor props untuk menerima argument yg dikirim ke footer
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return (
       <div>
         <h1>My Footer</h1>
+        {this.props.paragraf()}
       </div>
     );
   }
