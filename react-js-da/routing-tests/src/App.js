167039ba968a3a4192d693c2439601c3e73c0427
Abaljerind <abaljerinda@gmail.com>
2023-11-23 20:57:49 +0700

    import react-router-dom & add Home & About from Pages

--- react-js-da/routing-tests/src/App.js
+++ react-js-da/routing-tests/src/App.js
@@ -1,10 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
 
 const App = () => {
   return (
-    <div className="App">
-      <h3>Halaman Awal</h3>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
   );
 };
 
