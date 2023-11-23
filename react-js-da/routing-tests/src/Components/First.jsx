405641946e14c118e2c95b772664840cb0ab4c16
Abaljerind <abaljerinda@gmail.com>
2023-11-23 21:03:29 +0700

    add button & import useNavigate() to move from Home to About and back

--- react-js-da/routing-tests/src/Components/First.jsx
+++ react-js-da/routing-tests/src/Components/First.jsx
@@ -1,7 +1,11 @@
+import { useNavigate } from "react-router-dom";
 const First = () => {
+  const navigate = useNavigate();
+
   return (
     <>
       <h3>HOMEPAGE ROUTE</h3>
+      <button onClick={() => navigate("/about")}>Check About</button>
     </>
   );
 };
--- react-js-da/routing-tests/src/Components/Second.jsx
+++ react-js-da/routing-tests/src/Components/Second.jsx
@@ -1,7 +1,10 @@
+import { useNavigate } from "react-router-dom";
 const Second = () => {
+  const navigate = useNavigate();
   return (
     <>
       <h3>ABOUT PAGES</h3>
+      <button onClick={() => navigate("/")}>Back to Home!</button>
     </>
   );
 };
