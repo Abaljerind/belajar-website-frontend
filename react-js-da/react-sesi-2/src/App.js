7db003b57a534d445af539a0f21c6e0ef2169343
Abaljerind <abaljerinda@gmail.com>
2023-11-25 14:30:23 +0700

    contoh useEffect tahap 3: berdasarkan data tertentu yg berubah

--- react-js-da/react-sesi-2/src/App.js
+++ react-js-da/react-sesi-2/src/App.js
@@ -2,19 +2,26 @@
 import React, { useState, useEffect } from "react";
 
 function App() {
+  // useState
   // kiri -> getter
   // kanan -> setter
   // didalam tanda kurung useState() -> nilai default dari state yang kita punya
   // state -> anggap saja data sementara, bisa tipe data apa saja
   const [getPacarSaya, setPacarSaya] = useState(1);
 
+  const [getNamaPacar, setNamaPacar] = useState("");
   // useEffect() -> untuk render setiap perubahan object yang terjadi didalam browsernya.
   useEffect(() => {
-    alert("helo!");
-  }, []);
+    if (getPacarSaya === 1) {
+      setNamaPacar("Alisa");
+    } else {
+      setNamaPacar("uhuy");
+    }
+  }, [getPacarSaya]);
 
   return (
     <>
+      <h5>nama pacar: {getNamaPacar}</h5>
       <h1>saya punya: {getPacarSaya} pacar.</h1>
       {/* pada function setPacarSaya() dibawah, terdapat callback function untuk menambah data dengan parameter "previousData" */}
       <button onClick={() => setPacarSaya((previousData) => previousData + 1)}>
