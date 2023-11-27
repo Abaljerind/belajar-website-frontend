290414a690392c334712487c1fb2efdc1f021e96
Abaljerind <abaljerinda@gmail.com>
2023-11-27 14:01:20 +0700

    add content to App.js

--- react-js-da/movie-mania/src/App.js
+++ react-js-da/movie-mania/src/App.js
@@ -5,6 +5,19 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Movie Mania</h1>
+        <input
+          type="search"
+          placeholder="cari film yang anda inginkan..."
+          className="Movie-search"
+        />
+        <div className="Movie-container">
+          <div className="Movie-wrapper">
+            <div className="Movie-title">CONTOH PERTAMA</div>
+            <img className="Movie-image" src="" />
+            <div className="Movie-date">27-11-2023</div>
+            <div className="Movie-rate">8.0</div>
+          </div>
+        </div>
       </header>
     </div>
   );
