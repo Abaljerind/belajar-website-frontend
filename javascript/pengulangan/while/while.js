f4cd3b7c3997ccb924e141046bb321e3bbe312a0
Abaljerind <abaljerinda@gmail.com>
2023-12-18 13:23:08 +0700

    add while stop by program

--- javascript/pengulangan/while/while.js
+++ javascript/pengulangan/while/while.js
@@ -1,7 +1,15 @@
 // while yang dihentikan oleh user
-let ulang = true;
+// let ulang = true;
 
-while (ulang) {
-  console.log("Hello World");
-  ulang = confirm("Apa anda mau ulang?");
+// while (ulang) {
+//   console.log("Hello World");
+//   ulang = confirm("Apa anda mau ulang?");
+// }
+
+// while yang dihentikan oleh program
+let nilaiAwal = 1;
+
+while (nilaiAwal <= 1000) {
+  console.log(`Hello World! ${nilaiAwal}x`);
+  nilaiAwal++;
 }
