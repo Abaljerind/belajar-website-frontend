d5dd06749bb46f67533c53e07e8cb534ab4343b5
Abaljerind <abaljerinda@gmail.com>
2023-12-18 17:19:45 +0700

    create angkot4.js & add else-if

---
+++ javascript/pengulangan/latihan/angkot4.js
@@ -0,0 +1,13 @@
+let jumlahAngkot = 10;
+let angkotBeroperasi = 6;
+let noAngkot = 1;
+
+for (noAngkot; noAngkot <= jumlahAngkot; noAngkot++) {
+  if (noAngkot <= angkotBeroperasi) {
+    console.log(`Angkot No. ${noAngkot} beroperasi dengan baik.`);
+  } else if (noAngkot == 8) {
+    console.log(`Angkot No. ${noAngkot} sedang lembur.`);
+  } else {
+    console.log(`Angkot No. ${noAngkot} sedang tidak beroperasi.`);
+  }
+}
