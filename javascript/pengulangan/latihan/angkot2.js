1caf7146bd3f30150beaead665c525f2a5bc08c0
Abaljerind <abaljerinda@gmail.com>
2023-12-18 15:15:47 +0700

    add for loops, for looping

--- javascript/pengulangan/latihan/angkot2.js
+++ javascript/pengulangan/latihan/angkot2.js
@@ -1,7 +1,12 @@
 let jumlahAngkot = 10;
+let angkotBeroperasi = 6;
 let noAngkot = 1;
 
-while (noAngkot <= jumlahAngkot) {
+while (noAngkot <= angkotBeroperasi) {
   console.log(`Angkot No. ${noAngkot} beroperasi dengan baik.`);
   noAngkot++;
+}
+
+for (noAngkot = angkotBeroperasi + 1; noAngkot <= jumlahAngkot; noAngkot++) {
+  console.log(`Angkot No. ${noAngkot} sedang tidak beroperasi.`);
 }
