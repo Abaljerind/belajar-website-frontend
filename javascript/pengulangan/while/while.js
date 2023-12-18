828d4064412fa5a0032dd57607f99172d71cf0d2
Abaljerind <abaljerinda@gmail.com>
2023-12-18 13:59:57 +0700

    add exercise while

--- javascript/pengulangan/while/while.js
+++ javascript/pengulangan/while/while.js
@@ -7,9 +7,25 @@
 // }
 
 // while yang dihentikan oleh program
-let nilaiAwal = 1;
+// let nilaiAwal = 1;
 
-while (nilaiAwal <= 1000) {
-  console.log(`Hello World! ${nilaiAwal}x`);
-  nilaiAwal++;
+// while (nilaiAwal <= 1000) {
+//   console.log(`Hello World! ${nilaiAwal}x`);
+//   nilaiAwal++;
+// }
+
+// latihan
+// let nilaiAwal = 1;
+
+// while (nilaiAwal <= 225) {
+//   console.log(`Nilai awal bernilai ${nilaiAwal}.`);
+//   nilaiAwal = nilaiAwal + 3;
+// }
+// console.log(nilaiAwal);
+
+let nilaiAkhir = 221;
+
+while (nilaiAkhir >= 3) {
+  console.log(`Nilai akhirnya adalah nilai ${nilaiAkhir}.`);
+  nilaiAkhir = nilaiAkhir - 3;
 }
