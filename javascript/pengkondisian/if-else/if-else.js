854f573f448a4bf8e7038ec0789f60ecd56c0a32
Abaljerind <abaljerinda@gmail.com>
2023-12-18 17:15:35 +0700

    edit if-else to if-else-if

--- javascript/pengkondisian/if-else/if-else.js
+++ javascript/pengkondisian/if-else/if-else.js
@@ -1,7 +1,9 @@
 let angka = prompt("masukkan angka:");
 
 if (angka % 2 == 0) {
-  alert(`${angka} adalah bilangan Genap`);
+  alert(`${angka} adalah bilangan Genap.`);
+} else if (angka % 2 == 1) {
+  alert(`${angka} adalah bilangan Ganjil.`);
 } else {
-  alert(`${angka} adalah bilangan Ganjil`);
+  alert("yang anda masukkan bukan angka.");
 }
