51a7406cca0ee0e670b251d69a57a896007b641f
Abaljerind <abaljerinda@gmail.com>
2023-12-18 17:31:12 +0700

    add or operator in else-if

--- javascript/pengulangan/latihan/angkot5.js
+++ javascript/pengulangan/latihan/angkot5.js
@@ -5,7 +5,7 @@ let noAngkot = 1;
 for (noAngkot; noAngkot <= jumlahAngkot; noAngkot++) {
   if (noAngkot <= angkotBeroperasi) {
     console.log(`Angkot No. ${noAngkot} beroperasi dengan baik.`);
-  } else if (noAngkot == 8) {
+  } else if (noAngkot == 8 || noAngkot == 10) {
     console.log(`Angkot No. ${noAngkot} sedang lembur.`);
   } else {
     console.log(`Angkot No. ${noAngkot} sedang tidak beroperasi.`);
