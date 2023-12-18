ee44e2dcd67642d74e03d27bcafd489acbfe88f9
Abaljerind <abaljerinda@gmail.com>
2023-12-18 15:48:37 +0700

    add for & if else to show text in console

--- javascript/pengulangan/latihan/angkot3.js
+++ javascript/pengulangan/latihan/angkot3.js
@@ -2,11 +2,10 @@ let jumlahAngkot = 10;
 let angkotBeroperasi = 6;
 let noAngkot = 1;
 
-while (noAngkot <= angkotBeroperasi) {
-  console.log(`Angkot No. ${noAngkot} beroperasi dengan baik.`);
-  noAngkot++;
-}
-
-for (noAngkot = angkotBeroperasi + 1; noAngkot <= jumlahAngkot; noAngkot++) {
-  console.log(`Angkot No. ${noAngkot} sedang tidak beroperasi.`);
+for (noAngkot; noAngkot <= jumlahAngkot; noAngkot++) {
+  if (noAngkot <= angkotBeroperasi) {
+    console.log(`Angkot No. ${noAngkot} beroperasi dengan baik.`);
+  } else {
+    console.log(`Angkot No. ${noAngkot} sedang tidak beroperasi.`);
+  }
 }
