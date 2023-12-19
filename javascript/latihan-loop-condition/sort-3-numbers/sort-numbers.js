297adbf357280639606a97f4b26d8aafbe2a999a
Abaljerind <abaljerinda@gmail.com>
2023-12-19 14:26:23 +0700

    add conditions to sort number from small to big int

--- javascript/latihan-loop-condition/sort-3-numbers/sort-numbers.js
+++ javascript/latihan-loop-condition/sort-3-numbers/sort-numbers.js
@@ -1,22 +1,45 @@
-let a = parseInt(prompt("masukkan angka pertama :"));
-let b = parseInt(prompt("masukkan angka kedua :"));
-let c = parseInt(prompt("masukkan angka ketiga :"));
+// let a = parseInt(prompt("masukkan angka pertama :"));
+// let b = parseInt(prompt("masukkan angka kedua :"));
+// let c = parseInt(prompt("masukkan angka ketiga :"));
+let a = 5;
+let b = 10;
+let c = 3;
 
 // sorting dengan if, bandingkan nilainya, a dengan b dan a dengan c, lalu b dengan c dan b dengan a, terakhir c dengan a dan c dengan b
-if (a > b && a > c) {
-  if (b > c) {
+// if (a > b && a > c) {
+//   if (b > c) {
+//     alert(`${a}, ${b}, ${c}`);
+//   } else {
+//     alert(`${a}, ${c}, ${b}`);
+//   }
+// } else if (b > a && b > c) {
+//   if (a > c) {
+//     alert(`${b}, ${a}, ${c}`);
+//   } else {
+//     alert(`${b}, ${c}, ${a}`);
+//   }
+// } else if (c > a && c > b) {
+//   if (a > b) {
+//     alert(`${c}, ${a}, ${b}`);
+//   } else {
+//     alert(`${c}, ${b}, ${a}`);
+//   }
+// }
+
+if (a < b && a < c) {
+  if (b < c) {
     alert(`${a}, ${b}, ${c}`);
   } else {
     alert(`${a}, ${c}, ${b}`);
   }
-} else if (b > a && b > c) {
-  if (a > c) {
+} else if (b < a && b < c) {
+  if (a < c) {
     alert(`${b}, ${a}, ${c}`);
   } else {
     alert(`${b}, ${c}, ${a}`);
   }
-} else if (c > a && c > b) {
-  if (a > b) {
+} else if (c < a && c < b) {
+  if (a < b) {
     alert(`${c}, ${a}, ${b}`);
   } else {
     alert(`${c}, ${b}, ${a}`);
