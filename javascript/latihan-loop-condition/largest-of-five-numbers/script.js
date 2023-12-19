7c544c227bad1c24af9a122b2f62ba70acf1ac5e
Abaljerind <abaljerinda@gmail.com>
2023-12-20 06:31:56 +0700

    add else-if conditions to check the largest number of 5 numbers

---
+++ javascript/latihan-loop-condition/largest-of-five-numbers/script.js
@@ -0,0 +1,19 @@
+let a = -5;
+let b = -2;
+let c = -6;
+let d = 0;
+let e = -1;
+
+// output = 0
+
+if (a > b && a > c && a > d && a > e) {
+  alert(a);
+} else if (b > a && b > c && b > d && b > e) {
+  alert(b);
+} else if (c > a && c > b && c > d && c > e) {
+  alert(c);
+} else if (d > a && d > b && d > c && d > e) {
+  alert(d);
+} else if (e > a && e > b && e > c && e > d) {
+  alert(e);
+}
