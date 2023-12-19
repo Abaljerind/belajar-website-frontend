542db9a49bc97dd2c097b9e814042072d5f4d8cf
Abaljerind <abaljerinda@gmail.com>
2023-12-19 10:43:25 +0700

    add switch case

---
+++ javascript/pengkondisian/switch/switch.js
@@ -0,0 +1,16 @@
+let angka = prompt("masukkan angka :");
+
+switch (angka) {
+  case "1":
+    alert("anda memasukkan angka 1");
+    break;
+  case "2":
+    alert("anda memasukkan angka 2");
+    break;
+  case "3":
+    alert("anda memasukkan angka 3");
+    break;
+  default:
+    alert("angka yang anda masukkan salah");
+    break;
+}
