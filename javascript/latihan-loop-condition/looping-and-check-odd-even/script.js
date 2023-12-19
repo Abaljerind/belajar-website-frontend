fe69f7c778e18a2aa185afdd108b1674d9b80c69
Abaljerind <abaljerinda@gmail.com>
2023-12-20 06:40:59 +0700

    add for loop and if else to check odd & even

---
+++ javascript/latihan-loop-condition/looping-and-check-odd-even/script.js
@@ -0,0 +1,7 @@
+for (let nilaiAwal = 0; nilaiAwal <= 15; nilaiAwal++) {
+  if (nilaiAwal % 2 === 0) {
+    console.log(`${nilaiAwal} is even`);
+  } else {
+    console.log(`${nilaiAwal} is odd`);
+  }
+}
