39ab6a9ca94bfb87423e75ddc61620527aed11b2
Abaljerind <abaljerinda@gmail.com>
2023-12-19 14:12:01 +0700

    edit value a,b,c to prompt

--- javascript/latihan-loop-condition/sort-3-numbers/sort-numbers.js
+++ javascript/latihan-loop-condition/sort-3-numbers/sort-numbers.js
@@ -1,6 +1,6 @@
-let a = 0;
-let b = -1;
-let c = 4;
+let a = parseInt(prompt("masukkan angka pertama :"));
+let b = parseInt(prompt("masukkan angka kedua :"));
+let c = parseInt(prompt("masukkan angka ketiga :"));
 
 // sorting dengan if, bandingkan nilainya, a dengan b dan a dengan c, lalu b dengan c dan b dengan a, terakhir c dengan a dan c dengan b
 if (a > b && a > c) {
