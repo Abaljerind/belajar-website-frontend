248c05de58c1d39533efc2d775f5382a214fc1cb
Abaljerind <abaljerinda@gmail.com>
2023-12-21 17:07:12 +0700

    delete unneccesary file & code, add Introduction component to App.js

--- react-js-da/react-restart-1/src/App.js
+++ react-js-da/react-restart-1/src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Introduction from "./components/Introduction";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Sesi 1</h1>
+      <p>Halo saya belajar react js</p>
+      <Introduction />
     </div>
   );
 }
