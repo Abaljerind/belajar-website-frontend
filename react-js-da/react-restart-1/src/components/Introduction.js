11bd3b68b31b615ae3502f81d7a96b93f3bfa8fe
Abaljerind <abaljerinda@gmail.com>
2023-12-21 17:07:37 +0700

    create Introduction component

---
+++ react-js-da/react-restart-1/src/components/Introduction.js
@@ -0,0 +1,10 @@
+function Introduction() {
+  return (
+    <div>
+      <h1>Introduction</h1>
+      <p>Pengenalan, halo nama saya naruto uzumaki!</p>
+    </div>
+  );
+}
+
+export default Introduction;
