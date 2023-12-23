b4b50520947ff9e54016cd256eab010af35d1561
Abaljerind <abaljerinda@gmail.com>
2023-12-23 17:10:41 +0700

    add TextBody component

--- react-js-da/react-restart-1/src/App.js
+++ react-js-da/react-restart-1/src/App.js
@@ -1,11 +1,13 @@
 import Introduction from "./components/Introduction";
 import Navigation from "./components/Navigation";
+import TextBody from "./components/TextBody";
 
 function App() {
   return (
     <div>
       <Introduction />
       <Navigation />
+      <TextBody />
     </div>
   );
 }
