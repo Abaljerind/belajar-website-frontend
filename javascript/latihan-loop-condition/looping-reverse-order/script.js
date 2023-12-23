2eb1a57eb6c2906018aea5c9bd3adea8aa728977
Abaljerind <abaljerinda@gmail.com>
2023-12-23 14:20:18 +0700

    add program to show array with reversed order

---
+++ javascript/latihan-loop-condition/looping-reverse-order/script.js
@@ -0,0 +1,6 @@
+let arr = [43, "what", 9, true, "cannot", false, "be", 3, true];
+let arrReversed = arr.reverse();
+
+for (let i = 0; i < arrReversed.length; i++) {
+  console.log(arrReversed[i]);
+}
