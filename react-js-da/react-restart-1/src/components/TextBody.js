0f96ce7687688dd6918d1fe0b8c7b95b33c25447
Abaljerind <abaljerinda@gmail.com>
2023-12-23 17:10:59 +0700

    create TextBody component & add the codes

---
+++ react-js-da/react-restart-1/src/components/TextBody.js
@@ -0,0 +1,68 @@
+function TextBody() {
+  const textStyle = {
+    color: "blue",
+    textDecoration: "underline",
+  };
+
+  return (
+    <main>
+      <h1>Hello World Title!</h1>
+
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt unde,
+        autem dolorem ea enim voluptas, inventore consequatur qui nulla
+        recusandae, aperiam in quo corrupti? Cum quia natus quis ab veritatis
+        vitae qui. Id ullam nam, voluptatibus, quibusdam voluptas vero earum at
+        neque unde maiores eveniet doloremque sequi ratione aliquid consequuntur
+        architecto nulla? Voluptatum iste provident necessitatibus facere, vel
+        dolorem minus voluptate unde. Mollitia delectus recusandae explicabo
+        dolores quae sint nostrum optio dolor quisquam! Id assumenda error
+        aperiam aspernatur cupiditate tempora pariatur accusamus quae? Ipsam
+        explicabo exercitationem suscipit quis voluptatum similique unde ducimus
+        soluta quo animi dignissimos, minima eaque et quia laborum corrupti
+        nostrum fugit enim magni natus dolorum! Vero placeat harum sit iusto eos
+        optio dolore. Itaque, numquam. Sunt praesentium tempora atque at harum.
+        Laudantium, provident exercitationem nostrum ratione eum porro suscipit
+        odit ipsam quae eligendi consequuntur dicta accusamus totam repellendus
+        sit nesciunt ipsa sint corrupti at officia culpa! Minima, qui cum
+        assumenda asperiores maxime iusto perspiciatis laudantium tempora
+        dignissimos quo, similique doloremque neque repellendus eius hic nobis
+        aliquid! Culpa pariatur ullam fugiat architecto nemo, illum ex aliquam
+        assumenda esse accusantium! Porro voluptatum odit suscipit blanditiis
+        veniam, fugit facere laudantium nihil, eos tempore quod ratione
+        architecto unde, vel esse ex? Tenetur porro voluptatem doloribus,
+        molestias molestiae eius rerum in, maxime neque repellat nostrum
+        recusandae debitis fuga numquam quasi accusantium iure iste laborum
+        obcaecati aliquam, aperiam perspiciatis deserunt! Laudantium sit,
+        repellat adipisci nemo atque voluptatibus dolor explicabo ad, odio quis
+        optio vel? Reiciendis asperiores unde non rem doloribus maxime corporis
+        voluptates?
+      </p>
+
+      <ul style={textStyle}>
+        <li>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos
+          rem unde rerum!
+        </li>
+        <li>
+          Incidunt ullam laborum ex tempora similique, amet perspiciatis hic
+          tenetur architecto distinctio.
+        </li>
+        <li>
+          Maiores veniam commodi earum obcaecati exercitationem aperiam culpa
+          molestias magnam quibusdam laborum?
+        </li>
+        <li>
+          Beatae eius quia obcaecati omnis tempore et ullam impedit distinctio
+          adipisci nisi.
+        </li>
+        <li>
+          Perspiciatis ea quae quisquam sit incidunt veritatis eos asperiores in
+          numquam totam.
+        </li>
+      </ul>
+    </main>
+  );
+}
+
+export default TextBody;
