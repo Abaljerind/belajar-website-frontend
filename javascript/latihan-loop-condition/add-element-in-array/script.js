9354d7f81fa7f725011b4c03af3156cbf6971133
Abaljerind <abaljerinda@gmail.com>
2023-12-23 15:20:04 +0700

    add program to add up elements from 2 array

---
+++ javascript/latihan-loop-condition/add-element-in-array/script.js
@@ -0,0 +1,10 @@
+let arr_3 = [4, 6, 7, 4];
+let arr_4 = [8, 1, 9, 6];
+
+let arrTotal = [];
+
+for (let i = 0; i < arr_3.length; i++) {
+  arrTotal.push(arr_3[i] + arr_4[i]);
+}
+
+console.log(arrTotal);
