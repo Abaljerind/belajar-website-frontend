087452be00a20d9000523baf49aa36d4d17b2a10
Abaljerind <abaljerinda@gmail.com>
2023-12-26 17:25:51 +0700

    create Main component

---
+++ react-js-da/latihan-2-react-js/src/Components/Main.js
@@ -0,0 +1,19 @@
+function Main() {
+  return (
+    <>
+      <h1>World Wide Web</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+        excepturi nulla possimus incidunt, sint reiciendis mollitia unde quaerat
+        ducimus provident suscipit illum aspernatur, in vel?
+      </p>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt iure nobis
+        atque, asperiores molestiae culpa minus officia quo?
+      </p>
+    </>
+  );
+}
+
+export default Main;
