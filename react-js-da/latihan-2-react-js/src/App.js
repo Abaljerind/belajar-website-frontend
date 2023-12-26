9b8a417b7e185492280ccb1d58d307850e78f510
Abaljerind <abaljerinda@gmail.com>
2023-12-26 17:32:06 +0700

    add Content component & send props

--- react-js-da/latihan-2-react-js/src/App.js
+++ react-js-da/latihan-2-react-js/src/App.js
@@ -1,10 +1,20 @@
 import "./App.css";
+import Content from "./Components/Content";
 import Main from "./Components/Main";
 
 function App() {
   return (
     <>
       <Main />
+      <Content
+        title="What's out there?"
+        content="Pointers to the world's online information, subjects, W3 servers, etc."
+      />
+      <Content title="Help" content="on the browser you are using." />
+      <Content
+        title="Software Products"
+        content="A list of W3 project components and their current state. (e.g. Line Mode, X11 Viola, NeXTStep, Servers, Tools, etc.)"
+      />
     </>
   );
 }
