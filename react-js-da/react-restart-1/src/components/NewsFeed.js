29eb5a612b49f8dba3e409b49a1558c3f9a77c69
Abaljerind <abaljerinda@gmail.com>
2023-12-26 14:49:36 +0700

    create NewsFeed & import NewsList to NewsFeed

---
+++ react-js-da/react-restart-1/src/components/NewsFeed.js
@@ -0,0 +1,14 @@
+import NewsList from "./NewsList";
+
+function NewsFeed() {
+  return (
+    <>
+      <h2>Latest News</h2>
+      <NewsList title="February 27th - Dolor sit amet" />
+      <NewsList title="March 3rd - Lorem Ipsum" />
+      <NewsList title="July 7th - Gami Suscipit" />
+    </>
+  );
+}
+
+export default NewsFeed;
