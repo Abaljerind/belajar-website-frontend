611a69dc74a3918f32c73bfe5e970ed899a1c8f9
Abaljerind <abaljerinda@gmail.com>
2023-12-26 14:53:22 +0700

    create Footer component

---
+++ react-js-da/react-restart-1/src/components/Footer.js
@@ -0,0 +1,18 @@
+function Footer() {
+  return (
+    <>
+      <p>Copyright &copy; 2009 RTL This.</p>
+
+      <ul>
+        <li>
+          <a href="#">Privacy Policy</a>
+        </li>
+        <li>
+          <a href="#">Terms of Use</a>
+        </li>
+      </ul>
+    </>
+  );
+}
+
+export default Footer;
