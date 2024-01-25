d2231ae3f2a78934c31b7903e1fcb646be072e13
Abaljerind <abaljerinda@gmail.com>
2024-01-25 13:56:04 +0700

    add useState & handleClick with event

--- wpu-react-js/3-tic-tac-toe/src/App.jsx
+++ wpu-react-js/3-tic-tac-toe/src/App.jsx
@@ -1,7 +1,19 @@
 import { useState } from "react";
 
 function Square() {
-  return <button className="square">X</button>;
+  const [value, setValue] = useState("");
+
+  // handleClick digunakan untuk menghandle ketika button square di klik untuk memunculkan tampilan X / O nantinya.
+  // handleClick wajib dibuat didalam function Square() karna akan digunakan oleh function Square()
+  function handleClick() {
+    setValue("X");
+  }
+
+  return (
+    <button className="square" onClick={handleClick}>
+      {value}
+    </button>
+  );
 }
 
 export default function Board() {
