8a68380b5c697d846d1f01fcc2d5ec55b04e1b8b
Abaljerind <abaljerinda@gmail.com>
2024-01-25 18:05:26 +0700

    add function calculateWinner to decide the winner

--- wpu-react-js/3-tic-tac-toe/src/App.jsx
+++ wpu-react-js/3-tic-tac-toe/src/App.jsx
@@ -51,3 +51,28 @@ export default function Board() {
     </div>
   );
 }
+
+// fungsi bantuan, logika untuk mencari pemenang, bukan bagian dari komponen, isinya bukan fitur react
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const a = lines[i][0]; // 0
+    const b = lines[i][1]; // 1
+    const c = lines[i][2]; // 2
+
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return false;
+}
