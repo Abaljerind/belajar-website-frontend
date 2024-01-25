8628d0bb694267d0c8ca750fd885b2dbacdbfba2
Abaljerind <abaljerinda@gmail.com>
2024-01-25 16:46:24 +0700

    add lifting useState in Board() & eventHandler with event, edit some code in Square()

--- wpu-react-js/3-tic-tac-toe/src/App.jsx
+++ wpu-react-js/3-tic-tac-toe/src/App.jsx
@@ -1,33 +1,35 @@
 import { useState } from "react";
 
-function Square() {
-  const [value, setValue] = useState("");
-
-  // handleClick digunakan untuk menghandle ketika button square di klik untuk memunculkan tampilan X / O nantinya.
-  // handleClick wajib dibuat didalam function Square() karna akan digunakan oleh function Square()
-  function handleClick() {
-    setValue("X");
-  }
-
+function Square({ value, onSquareClick }) {
   return (
-    <button className="square" onClick={handleClick}>
+    <button className="square" onClick={onSquareClick}>
       {value}
     </button>
   );
 }
 
 export default function Board() {
+  // useState dibawah akan membuat array yang punya 9 space dengan nilai null. fill() digunakan untuk mengisi nilai pada array nya.
+  // cara dibawah ini disebut lifting useState, karena komponen utama nya (Board()) akan mengetahui apa saja perubahan yang terjadi pada Square(), komponen turunan.
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick(i) {
+    const nextSquares = squares.slice();
+    nextSquares[i] = "X";
+    setSquares(nextSquares);
+  }
+
   return (
     <div className="board">
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
+      <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+      <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+      <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+      <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+      <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+      <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+      <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+      <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+      <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
     </div>
   );
 }
