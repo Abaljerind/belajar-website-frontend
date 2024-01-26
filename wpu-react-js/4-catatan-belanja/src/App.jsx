69ff187c214edcb8e7062573fc9548cc84d2b522
Abaljerind <abaljerinda@gmail.com>
2024-01-26 21:26:04 +0700

    add useState name & quantity, access value from useState

--- wpu-react-js/4-catatan-belanja/src/App.jsx
+++ wpu-react-js/4-catatan-belanja/src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 // data belanja dalam bentuk array
 const groceryItems = [
   {
@@ -38,6 +40,31 @@ function Header() {
 
 // komponen Form
 function Form() {
+  const [name, setName] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  // function untuk menangani perubahan state
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    // guard clause => menjaga sebelum user melakukan sesuatu pada field input, jika masih kosong maka tidak ada data yang diinput.
+    if (name === "") {
+      return;
+    }
+
+    const newItem = {
+      name: name,
+      quantity: quantity,
+      checked: false,
+      id: Date.now(),
+    };
+    console.log(newItem);
+
+    // set ulang nilai name dan quantity nya
+    setName("");
+    setQuantity(1);
+  }
+
   // membuat array untuk jumlah barang yang mau di beli, _, i adalah parameter map, _ untuk nilai yang mau diinput didalam array tapi karna _ maka artinya dikosongkan, i untuk index nya, karna yang mau dipakai adalah index nya agar dibuat pengulangan dari 1 sampai 20, itu sebabnya i + 1 agar mulainya dari 1 sampai 20.
   const quantityNum = [...Array(20)].map((_, i) => (
     <option value={i + 1} key={i + 1}>
@@ -46,11 +73,21 @@ function Form() {
   ));
 
   return (
-    <form className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>Hari ini belanja apa kita?</h3>
       <div>
-        <select>{quantityNum}</select>
-        <input type="text" placeholder="nama barang..." />
+        <select
+          value={quantity}
+          onChange={(e) => setQuantity(Number(e.target.value))}
+        >
+          {quantityNum}
+        </select>
+        <input
+          type="text"
+          placeholder="nama barang..."
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
       </div>
       <button>Tambah</button>
     </form>
