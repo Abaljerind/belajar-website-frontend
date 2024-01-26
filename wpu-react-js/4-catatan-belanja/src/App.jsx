dc044332c44a0a5fa25a46f99505b71f3995badc
Abaljerind <abaljerinda@gmail.com>
2024-01-26 16:27:05 +0700

    create quantityNum for looping index array for quantity of item

--- wpu-react-js/4-catatan-belanja/src/App.jsx
+++ wpu-react-js/4-catatan-belanja/src/App.jsx
@@ -38,17 +38,18 @@ function Header() {
 
 // komponen Form
 function Form() {
+  // membuat array untuk jumlah barang yang mau di beli, _, i adalah parameter map, _ untuk nilai yang mau diinput didalam array tapi karna _ maka artinya dikosongkan, i untuk index nya, karna yang mau dipakai adalah index nya agar dibuat pengulangan dari 1 sampai 20, itu sebabnya i + 1 agar mulainya dari 1 sampai 20.
+  const quantityNum = [...Array(20)].map((_, i) => (
+    <option value={i + 1} key={i + 1}>
+      {i + 1}
+    </option>
+  ));
+
   return (
     <form className="add-form">
       <h3>Hari ini belanja apa kita?</h3>
       <div>
-        <select>
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option>
-          <option value="5">5</option>
-        </select>
+        <select>{quantityNum}</select>
         <input type="text" placeholder="nama barang..." />
       </div>
       <button>Tambah</button>
