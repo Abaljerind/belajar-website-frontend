a7c7070954e4c36b894592c95d9426a03a94c6a4
Abaljerind <abaljerinda@gmail.com>
2024-01-26 10:26:22 +0700

    Move code to each component + create components local

--- wpu-react-js/4-catatan-belanja/src/App.jsx
+++ wpu-react-js/4-catatan-belanja/src/App.jsx
@@ -1,21 +1,43 @@
 export default function App() {
   return (
     <div className="app">
-      <h1>Catatan Belanjaku 📝</h1>
-      <form className="add-form">
-        <h3>Hari ini belanja apa kita?</h3>
-        <div>
-          <select>
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-          </select>
-          <input type="text" placeholder="nama barang..." />
-        </div>
-        <button>Tambah</button>
-      </form>
+      <Header />
+      <Form />
+      <GroceryList />
+      <Footer />
+    </div>
+  );
+}
+
+// komponen Header
+function Header() {
+  return <h1>Catatan Belanjaku 📝</h1>;
+}
+
+// komponen Form
+function Form() {
+  return (
+    <form className="add-form">
+      <h3>Hari ini belanja apa kita?</h3>
+      <div>
+        <select>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+        </select>
+        <input type="text" placeholder="nama barang..." />
+      </div>
+      <button>Tambah</button>
+    </form>
+  );
+}
+
+// komponen GroceryList
+function GroceryList() {
+  return (
+    <>
       <div className="list">
         <ul>
           <li>
@@ -43,9 +65,15 @@ export default function App() {
         </select>
         <button>Bersihkan Daftar</button>
       </div>
-      <footer className="stats">
-        Ada 10 barang di daftar belanjaan, 5 barang sudah dibeli (50%)
-      </footer>
-    </div>
+    </>
+  );
+}
+
+// komponen footer
+function Footer() {
+  return (
+    <footer className="stats">
+      Ada 10 barang di daftar belanjaan, 5 barang sudah dibeli (50%)
+    </footer>
   );
 }
