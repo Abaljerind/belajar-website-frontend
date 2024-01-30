d16aec5d0578059856db4ae0c4213fc3238337f7
Abaljerind <abaljerinda@gmail.com>
2024-01-30 14:16:21 +0700

    add sorting function to sort items

--- wpu-react-js/4-catatan-belanja/src/App.jsx
+++ wpu-react-js/4-catatan-belanja/src/App.jsx
@@ -128,11 +128,26 @@ function Form({ onAddItem }) {
 
 // komponen GroceryList
 function GroceryList({ items, onDeleteItem, onToggleItem, onClearItems }) {
+  const [sortBy, setSortBy] = useState("input");
+
+  let sortedItems; // untuk array baru agar array items tidak ditimpa datanya.
+  switch (sortBy) {
+    case "name":
+      sortedItems = items.slice().sort((a, b) => a.name.localeCompare(b.name));
+      break;
+    case "checked":
+      sortedItems = items.slice().sort((a, b) => a.checked - b.checked);
+      break;
+    default:
+      sortedItems = items;
+      break;
+  }
+
   return (
     <>
       <div className="list">
         <ul>
-          {items.map((item) => (
+          {sortedItems.map((item) => (
             // mengirim props nya ke komponen Item
             <Item
               item={item}
@@ -144,7 +159,7 @@ function GroceryList({ items, onDeleteItem, onToggleItem, onClearItems }) {
         </ul>
       </div>
       <div className="actions">
-        <select>
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
           <option value="input">Urutkan berdasarkan urutan input</option>
           <option value="name">Urutkan berdasarkan nama barang</option>
           <option value="checked">Urutkan berdasarkan ceklis</option>
