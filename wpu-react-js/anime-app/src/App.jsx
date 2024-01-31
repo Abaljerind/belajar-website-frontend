4d6ba4876160579f053f7173ec85d9e77588b64d
Abaljerind <abaljerinda@gmail.com>
2024-01-31 11:31:15 +0700

    create component: AnimeList, Anime

--- wpu-react-js/anime-app/src/App.jsx
+++ wpu-react-js/anime-app/src/App.jsx
@@ -129,24 +129,39 @@ function ListBox({ animes, onSelectedAnime }) {
         {isOpen1 ? "–" : "+"}
       </button>
       {isOpen1 && (
-        <ul className="list list-anime">
-          {animes?.map((anime) => (
-            <li
-              key={anime.mal_id}
-              onClick={() => onSelectedAnime(anime.mal_id)}
-            >
-              <img src={anime.image} alt={`${anime.title} cover`} />
-              <h3>{anime.title}</h3>
-              <div>
-                <p>
-                  <span>{anime.year}</span>
-                </p>
-              </div>
-            </li>
-          ))}
-        </ul>
+        <AnimeList animes={animes} onSelectedAnime={onSelectedAnime} />
       )}
     </div>
+  );
+}
+
+// component AnimeList
+function AnimeList({ animes, onSelectedAnime }) {
+  return (
+    <ul className="list list-anime">
+      {animes?.map((anime) => (
+        <Anime
+          key={anime.mal_id}
+          anime={anime}
+          onSelectedAnime={onSelectedAnime}
+        />
+      ))}
+    </ul>
+  );
+}
+
+// component Anime
+function Anime({ anime, onSelectedAnime }) {
+  return (
+    <li onClick={() => onSelectedAnime(anime.mal_id)}>
+      <img src={anime.image} alt={`${anime.title} cover`} />
+      <h3>{anime.title}</h3>
+      <div>
+        <p>
+          <span>{anime.year}</span>
+        </p>
+      </div>
+    </li>
   );
 }
 
