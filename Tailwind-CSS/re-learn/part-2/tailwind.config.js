42eb8de38d54b10259c3e1b91ab5ca4ab0e434f2
Abaljerind <abaljerinda@gmail.com>
2024-06-28 21:42:05 +0700

    re-learn part-4

--- Tailwind-CSS/re-learn/part-2/tailwind.config.js
+++ Tailwind-CSS/re-learn/part-2/tailwind.config.js
@@ -14,6 +14,20 @@ module.exports = {
         wpu: "#bada55",
         kopi: "#c0ffee",
       },
+      animation: {
+        "spin-slow": "spin 3s linear infinite",
+        wiggle: "wiggle 1s ease-in-out infinite",
+      },
+      keyframes: {
+        wiggle: {
+          "0%, 100%": {
+            transform: "rotate(-3deg)",
+          },
+          "50%": {
+            transform: "rotate(3deg)",
+          },
+        },
+      },
     },
   },
   plugins: [],
