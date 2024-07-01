a44fa48ba0090255f4417de7fe4a03124ac512d1
Abaljerind <abaljerinda@gmail.com>
2024-07-01 18:32:04 +0700

    update dark mode + perbaikan fitur

--- Tailwind-CSS/re-learn/part-2/portfolio-tailwind/dist/js/script.js
+++ Tailwind-CSS/re-learn/part-2/portfolio-tailwind/dist/js/script.js
@@ -7,16 +7,54 @@ hamburger.addEventListener("click", function () {
   navMenu.classList.toggle("hidden");
 });
 
+// klik diluar hamburger
+window.addEventListener("click", function (e) {
+  if (e.target != hamburger && e.target != navMenu) {
+    hamburger.classList.remove("hamburger-active");
+    navMenu.classList.add("hidden");
+  }
+});
+
 // navbar-fixed
 window.onscroll = function () {
   const header = document.querySelector("header");
+  const toTop = document.getElementById("to-top");
 
   //   mengambil nilai jarak dari posisi header terhadap top nya
   const fixedNav = header.offsetTop;
 
   if (window.scrollY > fixedNav) {
     header.classList.add("navbar-fixed");
+    toTop.classList.remove("hidden");
+    toTop.classList.add("flex");
   } else {
     header.classList.remove("navbar-fixed");
+    toTop.classList.remove("flex");
+    toTop.classList.add("hidden");
   }
 };
+
+// dark mode
+const darkToggle = document.getElementById("dark-toggle");
+const html = document.querySelector("html");
+
+darkToggle.addEventListener("click", function () {
+  if (darkToggle.checked) {
+    html.classList.add("dark");
+    localStorage.theme = "dark";
+  } else {
+    html.classList.remove("dark");
+    localStorage.theme = "light";
+  }
+});
+
+// memindahkan posisi toggle sesuai mode
+if (
+  localStorage.theme === "dark" ||
+  (!("theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  darkToggle.checked = true;
+} else {
+  darkToggle.checked = false;
+}
