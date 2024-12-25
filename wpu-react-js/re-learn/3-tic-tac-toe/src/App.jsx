881071377fd63f85819aac77b4b21a3e48442975
Abaljerind <abaljerinda@gmail.com>
2024-12-25 16:47:23 +0700

    Create function to calculate the winner
    Create calculateWinner() with looping to go through every squares,
    So that we can know who the winner is, X or O
    Then add new condition if the winner already decided,
    No Squares should be allowed to be clicked anymore
    Create variable winner and status to display the winner with new div

--- wpu-react-js/re-learn/3-tic-tac-toe/src/App.jsx
+++ wpu-react-js/re-learn/3-tic-tac-toe/src/App.jsx
@@ -14,7 +14,7 @@ function Board() {
 
   function hanldeClick(i) {
     // cek apakah squares nya sudah ada isinya atau belum, kalau sudah ada jangan diubah
-    if (squares[i]) return;
+    if (squares[i] || calculateWinner(squares)) return;
 
     const nextSquares = squares.slice();
     // cek apakah xIsNext bernilai true? untuk mengganti giliran antara X dan O
@@ -29,19 +29,59 @@ function Board() {
     setXIsNext(!xIsNext);
   }
 
+  const winner = calculateWinner(squares);
+  let status = "";
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+
   return (
-    <div className="board">
-      <Square value={squares[0]} onSquareClick={() => hanldeClick(0)} />
-      <Square value={squares[1]} onSquareClick={() => hanldeClick(1)} />
-      <Square value={squares[2]} onSquareClick={() => hanldeClick(2)} />
-      <Square value={squares[3]} onSquareClick={() => hanldeClick(3)} />
-      <Square value={squares[4]} onSquareClick={() => hanldeClick(4)} />
-      <Square value={squares[5]} onSquareClick={() => hanldeClick(5)} />
-      <Square value={squares[6]} onSquareClick={() => hanldeClick(6)} />
-      <Square value={squares[7]} onSquareClick={() => hanldeClick(7)} />
-      <Square value={squares[8]} onSquareClick={() => hanldeClick(8)} />
-    </div>
+    <>
+      <div className="status">{status}</div>
+      <div className="board">
+        <Square value={squares[0]} onSquareClick={() => hanldeClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => hanldeClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => hanldeClick(2)} />
+        <Square value={squares[3]} onSquareClick={() => hanldeClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => hanldeClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => hanldeClick(5)} />
+        <Square value={squares[6]} onSquareClick={() => hanldeClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => hanldeClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => hanldeClick(8)} />
+      </div>
+    </>
   );
+}
+
+// fungsi pencari pemenang
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    // const a = lines[i][0]; // looping pertama, 0
+    // const b = lines[i][1]; // looping kedua, 1
+    // const c = lines[i][2]; // looping ketiga, 2
+
+    // dengan teknik destructuring
+    const [a, b, c] = lines[i];
+
+    if (squares[a] && squares[a] === squares[b] && squares[c]) {
+      return squares[a];
+    }
+  }
+
+  return false;
 }
 
 export default Board;
