6145d7da323d58900618744aed93ff7926e8017e
Abaljerind <abaljerinda@gmail.com>
2024-12-25 14:41:01 +0700

    Use lifting up state
    Remove handleClick inside Square component
    Add new useState with value of 9 null with Array data type
    Create new handleClick inside Board component
    Add onSquareClick with anonymous function for handleClick()

--- wpu-react-js/re-learn/3-tic-tac-toe/src/App.jsx
+++ wpu-react-js/re-learn/3-tic-tac-toe/src/App.jsx
@@ -1,31 +1,33 @@
 import { useState } from "react";
 
-function Square() {
-  const [value, setValue] = useState("");
-
-  function handleClick() {
-    setValue("X");
-  }
-
+function Square({ value, onSquareClick }) {
   return (
-    <button className="square" onClick={handleClick}>
+    <button className="square" onClick={onSquareClick}>
       {value}
     </button>
   );
 }
 
 function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function hanldeClick(i) {
+    const nextSquares = squares.slice();
+    nextSquares[i] = "X";
+    setSquares(nextSquares);
+  }
+
   return (
     <div className="board">
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
+      <Square value={squares[0]} onSquareClick={() => hanldeClick(0)} />
+      <Square value={squares[1]} onSquareClick={() => hanldeClick(1)} />
+      <Square value={squares[2]} onSquareClick={() => hanldeClick(2)} />
+      <Square value={squares[3]} onSquareClick={() => hanldeClick(3)} />
+      <Square value={squares[4]} onSquareClick={() => hanldeClick(4)} />
+      <Square value={squares[5]} onSquareClick={() => hanldeClick(5)} />
+      <Square value={squares[6]} onSquareClick={() => hanldeClick(6)} />
+      <Square value={squares[7]} onSquareClick={() => hanldeClick(7)} />
+      <Square value={squares[8]} onSquareClick={() => hanldeClick(8)} />
     </div>
   );
 }
