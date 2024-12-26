47e968be94a2147b171b20275b307db8ef640552
Abaljerind <abaljerinda@gmail.com>
2024-12-26 11:47:38 +0700

    Create looping with Map to loop HTML tag select
    This tag select will be used for display and select quantity of item

--- wpu-react-js/re-learn/4-catatan-belanja/src/App.jsx
+++ wpu-react-js/re-learn/4-catatan-belanja/src/App.jsx
@@ -35,17 +35,16 @@ function Header() {
 }
 
 function Form() {
+  const quantityNum = [...Array(20)].map((_, index) => (
+    <option value={index + 1} key={index + 1}>
+      {index + 1}
+    </option>
+  ));
   return (
     <form className="add-form">
       <h3>Hari ini belanja apa kita?</h3>
       <div>
-        <select>
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option>
-          <option value="5">5</option>
-        </select>
+        <select>{quantityNum}</select>
         <input type="text" placeholder="nama barang..." />
       </div>
       <button>Tambah</button>
