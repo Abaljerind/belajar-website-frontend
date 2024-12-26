9b4e77c87f5161ce5eec49f418d20fd3058b87a4
Abaljerind <abaljerinda@gmail.com>
2024-12-26 14:57:05 +0700

    Create new handleDeleteItem()
    This handle will filter item from array, then display it
    Then send the onDeleteItem to GroceryList using props
    From GroceryList, onDeleteItem sent to item using props again
    Then onDeleteItem added to button in event onClick

--- wpu-react-js/re-learn/4-catatan-belanja/src/App.jsx
+++ wpu-react-js/re-learn/4-catatan-belanja/src/App.jsx
@@ -28,11 +28,15 @@ export default function App() {
     setItems([...items, item]);
   }
 
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
   return (
     <div className="app">
       <Header />
       <Form onAddItem={handleAddItem} />
-      <GroceryList items={items} />
+      <GroceryList items={items} onDeleteItem={handleDeleteItem} />
       <Footer />
     </div>
   );
@@ -83,13 +87,13 @@ function Form({ onAddItem }) {
   );
 }
 
-function GroceryList({ items }) {
+function GroceryList({ items, onDeleteItem }) {
   return (
     <>
       <div className="list">
         <ul>
           {items.map((item) => (
-            <Item item={item} key={item.id} />
+            <Item item={item} key={item.id} onDeleteItem={onDeleteItem} />
           ))}
         </ul>
       </div>
@@ -109,14 +113,14 @@ function Footer() {
   return <footer className="stats">Ada 10 barang di daftar belanjaan, 5 barang sudah dibeli (50%)</footer>;
 }
 
-function Item({ item }) {
+function Item({ item, onDeleteItem }) {
   return (
     <li key={item.id}>
       <input type="checkbox" />
       <span style={item.checked ? { textDecoration: "line-through" } : {}}>
         {item.itemQuantity} {item.itemName}
       </span>
-      <button>&times;</button>
+      <button onClick={() => onDeleteItem(item.id)}>&times;</button>
     </li>
   );
 }
