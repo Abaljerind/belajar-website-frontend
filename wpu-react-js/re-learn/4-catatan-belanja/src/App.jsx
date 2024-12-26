637a413f138d2ef332394d81c44ea1d8c9958b10
Abaljerind <abaljerinda@gmail.com>
2024-12-26 13:16:58 +0700

    Add useState for itemName and ItemQuantity
    Create handleSubmit for tag form, to submit data from tag input & select
    Add attr value and onChange to tag input & select to insert the data
    Add guard clause for prevent empty string from tag input to be submitted

--- wpu-react-js/re-learn/4-catatan-belanja/src/App.jsx
+++ wpu-react-js/re-learn/4-catatan-belanja/src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const groceryItems = [
   {
     id: 1,
@@ -35,17 +37,40 @@ function Header() {
 }
 
 function Form() {
+  const [itemName, setItemName] = useState("");
+  const [itemQuantity, setItemQuantity] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    // guard clause -> untuk pengecekan apakah itemName ada isinya atau cuma string kosong, kalau kosong langsung keluar dari function ini sehingga data tidak dimasukkan.
+    if (!itemName) return;
+
+    const newItem = { itemName, itemQuantity, checked: false, id: Date.now() };
+    console.log(newItem);
+
+    setItemName("");
+    setItemQuantity(1);
+  }
+
   const quantityNum = [...Array(20)].map((_, index) => (
     <option value={index + 1} key={index + 1}>
       {index + 1}
     </option>
   ));
   return (
-    <form className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>Hari ini belanja apa kita?</h3>
       <div>
-        <select>{quantityNum}</select>
-        <input type="text" placeholder="nama barang..." />
+        <select value={itemQuantity} onChange={(e) => setItemQuantity(+e.target.value)}>
+          {quantityNum}
+        </select>
+        <input
+          type="text"
+          placeholder="nama barang..."
+          value={itemName}
+          onChange={(e) => setItemName(e.target.value)}
+        />
       </div>
       <button>Tambah</button>
     </form>
