d06251523489870a9be0cf6adfa31c6b8b05bd81
Abaljerind <abaljerinda@gmail.com>
2025-07-05 10:38:02 +0700

    Kompilasi typescript, include & exclude

---
+++ typescript/belajar-typescript-dasar/dist/src/say-hello.js
@@ -0,0 +1,5 @@
+export function sayHello(name) {
+    return `Hello ${name}`;
+}
+console.log(sayHello("naruto"));
+console.log(sayHello("sasuke"));
---
+++ typescript/belajar-typescript-dasar/dist/tests/hello.test.js
@@ -0,0 +1,7 @@
+"use strict";
+describe("Hello", function () {
+    it("should say hello", function () {
+        const name = "Hello Naruto";
+        expect(name).toBe("Hello Naruto");
+    });
+});
