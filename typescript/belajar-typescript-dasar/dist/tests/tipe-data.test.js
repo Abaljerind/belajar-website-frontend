b8c24a1cebd0ae055b8a70f9d45e3e4161d13b78
Abaljerind <abaljerinda@gmail.com>
2025-07-05 10:52:13 +0700

    Tipe data primitif

---
+++ typescript/belajar-typescript-dasar/dist/tests/tipe-data.test.js
@@ -0,0 +1,11 @@
+"use strict";
+describe("Data Type", () => {
+    it("should must declare", () => {
+        const name = "Naruto Uzumaki";
+        const balance = 1000000;
+        const isVip = true;
+        console.info(name);
+        console.info(balance);
+        console.info(isVip);
+    });
+});
---
+++ typescript/belajar-typescript-dasar/tests/tipe-data.test.ts
@@ -0,0 +1,11 @@
+describe("Data Type", () => {
+  it("should must declare", () => {
+    const name: string = "Naruto Uzumaki";
+    const balance: number = 1000000;
+    const isVip: boolean = true;
+
+    console.info(name);
+    console.info(balance);
+    console.info(isVip);
+  });
+});
