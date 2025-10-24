9ff09bb28a96fd9a33aad794bb57f334ea8a483c
Abaljerind <abaljerinda@gmail.com>
2025-10-24 10:52:03 +0700

    Belajar function parameter

--- typescript/belajar-typescript-dasar/tests/function.test.ts
+++ typescript/belajar-typescript-dasar/tests/function.test.ts
@@ -12,4 +12,39 @@ describe("function", function () {
 
     printHello("Sasuke");
   });
+
+  it("should support default value", function () {
+    function sayHello(name: string = "Guest"): string {
+      return `Hello ${name}`;
+    }
+
+    expect(sayHello()).toBe("Hello Guest");
+    expect(sayHello("Sakura")).toBe("Hello Sakura");
+  });
+
+  it("should support rest parameter", function () {
+    function sum(...values: number[]): number {
+      let total = 0;
+      for (const value of values) {
+        total += value;
+      }
+
+      return total;
+    }
+
+    expect(sum(1, 2, 3, 4, 5)).toBe(15);
+  });
+
+  it("should support optional parameter", function () {
+    function sayHello(firstname: string, lastname?: string): string {
+      if (lastname) {
+        return `Hello ${firstname} ${lastname}`;
+      } else {
+        return `Hello ${firstname}`;
+      }
+    }
+
+    expect(sayHello("Naruto")).toBe("Hello Naruto");
+    expect(sayHello("Naruto", "Uzumaki")).toBe("Hello Naruto Uzumaki");
+  });
 });
